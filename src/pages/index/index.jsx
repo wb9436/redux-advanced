787fd867627edf4fac36e0077d28555c6e870c54
wb9436 <1016065991@qq.com>
2019-07-22 15:22:52 +0800

    。。。

--- src/pages/index/index.jsx
+++ src/pages/index/index.jsx
@@ -26,10 +26,6 @@ class Index extends Component {
     navigationBarTitleText: '首页'
   }
 
-  componentWillReceiveProps(nextProps) {
-    console.log(this.props, nextProps)
-  }
-
   componentWillUnmount() {
   }
 
@@ -39,12 +35,32 @@ class Index extends Component {
   componentDidHide() {
   }
 
+  onTestPromise() {
+    this.onPromiseClick().then(data => {
+      console.log('已接收promise 执行结果', data, Date.now())
+    })
+  }
+
+  onPromiseClick() {
+    console.log('promise 开始执行', Date.now())
+    return new Promise(((resolve, reject) => { //reject就是失败的时候的回调
+      try {
+        setTimeout(() => {
+          console.log('promise 执行完毕', Date.now())
+          resolve('promise已经执行完毕')
+        }, 2000)
+      } catch (e) {
+      }
+    }))
+  }
+
   render() {
     return (
       <View className='index'>
         <Button className='add_btn' onClick={this.props.add}>+</Button>
         <Button className='dec_btn' onClick={this.props.dec}>-</Button>
         <Button className='dec_btn' onClick={this.props.asyncAdd}>async</Button>
+        <Button className='dec_btn' onClick={this.onTestPromise.bind(this)}>Test Promise</Button>
         <View><Text>{this.props.counter.num}</Text></View>
         <View><Text>Hello, World</Text></View>
       </View>
